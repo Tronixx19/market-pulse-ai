e859f42d3c4849eee31f60a4c61b47f319bbee91
TroniXx19 <danielalonsodiaza@gmail.com>
2026-01-04 02:30:49 -0300

    -´

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import { createSignal, onMount, onCleanup, Show, For } from 'solid-js';
-import PriceChart from './Chart';
+import PriceChart from './chart';
 
 interface TradeData {
   symbol: string;
